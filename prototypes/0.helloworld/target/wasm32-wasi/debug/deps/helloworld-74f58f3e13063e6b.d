f044bf8aee1e3105ae7f5e0e8a50f7c14bec243f
ilcors-dev <corsetti.luca28@gmail.com>
2023-01-12 20:50:16 +0100

    added first wasi test

---
+++ prototypes/0.helloworld/target/wasm32-wasi/debug/deps/helloworld-74f58f3e13063e6b.d
@@ -0,0 +1,5 @@
+/Users/ilcors-dev/Desktop/projects/bachelor-thesis/prototypes/0.helloworld/target/wasm32-wasi/debug/deps/helloworld-74f58f3e13063e6b.wasm: src/main.rs
+
+/Users/ilcors-dev/Desktop/projects/bachelor-thesis/prototypes/0.helloworld/target/wasm32-wasi/debug/deps/helloworld-74f58f3e13063e6b.d: src/main.rs
+
+src/main.rs:
---
+++ prototypes/0.helloworld/target/wasm32-wasi/debug/helloworld.d
@@ -0,0 +1,1 @@
+/Users/ilcors-dev/Desktop/projects/bachelor-thesis/prototypes/0.helloworld/target/wasm32-wasi/debug/helloworld.wasm: /Users/ilcors-dev/Desktop/projects/bachelor-thesis/prototypes/0.helloworld/src/main.rs
